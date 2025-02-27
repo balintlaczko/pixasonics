0c9495c7dc1399c9b8ad39f8a4d554b05e5bcfd9
Balint Laczko <45127463+balintlaczko@users.noreply.github.com>
2025-02-27 19:52:21 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='pixasonics',
-    version='0.1.3',
+    version='0.1.4',
     author='Balint Laczko',
     author_email='balint.laczko@imv.uio.no',
     description='An Image Sonification Toolbox',
